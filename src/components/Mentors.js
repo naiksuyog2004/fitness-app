fa2f7d19554c448e919db47321a4f638b73f9a47
Suyog Naik <naiksuyogsudhir@gmail.com>
2024-04-06 12:05:21 +0530

    Final commit

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -44,19 +44,24 @@ const Mentors = () => {
                 <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
                     Our Mentors
                 </Typography>
-                <Button
-                    variant='contained'
-                    color='error'
-                    onClick={handleOpenForm}
-                    sx={{ marginRight: '40px' }}
-                >
-                    Be a Mentor
-                </Button>
+                <div>
+                    <Button
+                        variant='contained'
+                        color='error'
+                        onClick={handleOpenForm}
+                        sx={{ marginRight: '40px' }}
+                    >
+                        Be a Mentor
+                    </Button>
+
+                </div>
             </Box>
             {showForm && <MentorForm onCloseForm={handleCloseForm} onSubmitForm={handleSubmitForm} />}
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: '20px' }}>
                 {mentors.map((mentor) => (
-                    <Card sx={{ marginBottom: '20px', width: '100%', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }}>
+                    <Card key={mentor.id} sx={{ marginBottom: '20px', width: '100%', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)', border: '1px solid #ccc', transition: 'box-shadow 0.3s ease-in-out' }}
+                        onMouseOver={(e) => { e.currentTarget.style.boxShadow = '0px 4px 8px rgba(0, 0, 0, 0.2)' }}
+                        onMouseOut={(e) => { e.currentTarget.style.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.1)' }}>
                         <CardContent>
                             <Typography variant="h5" sx={{ marginBottom: '10px' }}>{mentor.name}</Typography>
                             <Typography variant="subtitle1" sx={{ marginBottom: '8px', color: 'rgba(0, 0, 0, 0.6)' }}>
@@ -68,13 +73,18 @@ const Mentors = () => {
                             <Typography variant="subtitle1" sx={{ marginBottom: '8px', color: 'rgba(0, 0, 0, 0.6)' }}>
                                 Speciality: {mentor.speciality}
                             </Typography>
-                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                <Typography variant="subtitle1" sx={{ marginRight: '8px', color: 'rgba(0, 0, 0, 0.6)' }}>
-                                    Price:
-                                </Typography>
-                                <Typography variant="h6" sx={{ color: '#1e88e5' }}>
-                                    ₹ {mentor.price.toLocaleString()} INR
-                                </Typography>
+                            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                <Box>
+                                    <Typography variant="subtitle1" sx={{ display: 'flex', color: 'rgba(0, 0, 0, 0.6)' }}>
+                                        Price:
+                                        <Typography variant="h6" sx={{ margin: '2px', color: '#1e88e5' }}>
+                                            ₹ {mentor.price.toLocaleString()} INR
+                                        </Typography>
+                                    </Typography>
+                                </Box>
+                                <Button variant="contained" color="success" sx={{ alignSelf: 'flex-end' }}>
+                                    Book
+                                </Button>
                             </Box>
                         </CardContent>
                     </Card>
